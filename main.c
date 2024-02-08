55c32f2f660392a752e74b48cfe3b2597dbcd5eb
Alan BOULESTEIX <alan.boulesteix@gmail.com>
2024-02-08 08:29:00 +0100

    Add small test function in main.c + add strcmp.s file [empty]

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: aboulest <aboulest@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 10:05:03 by aboulest          #+#    #+#             */
-/*   Updated: 2024/02/07 17:08:09 by aboulest         ###   ########.fr       */
+/*   Updated: 2024/02/08 08:27:40 by aboulest         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,27 @@ void	ft_strelenTest(char *str)
 	size_t	len_og;
 	size_t	len_new;
 
+	printf("=================STRLEN====================");
 	len_og = strlen(str);
 	len_new = ft_strlen(str);
 	printf("len_og = %ld\n", len_og);
 	printf("len_new = %ld\n", len_new);
+	printf("===========================================");
 
 }
 
-void	ft_strcpyTest() {
+void	ft_strcpyTest(char *str) {
+	char	dest[100];
+	char	ft_dest[100];
 	
+	printf("=================STRCPY====================");
+	printf("[AVANT] real: %s\n", dest);
+	strcpy(dest, str);
+	printf("[APRES] real: %s\n", dest);
+	printf("[AVANT] mine: %s\n", ft_dest);
+	ft_strcpy(ft_dest, str);
+	printf("[APRES] mine: %s\n", ft_dest);	
+	printf("===========================================");
 }
 
 int	main(int ac, char **av)
@@ -33,17 +45,9 @@ int	main(int ac, char **av)
 
 	if (ac != 2)
 	{
-		printf("\033[1;31m%s\033[0m", "USE: libasm [ ] ");
-		
+		printf("\033[1;31m%s\033[0m", "USE: libasm [string] ");
+		return (1);
 	}
-	char	dest[100];
-	char	ft_dest[100];
-	
-	printf("[AVANT] %s\n", dest);
-	strcpy(dest, av[1]);
-	printf("[APRES] %s\n", dest);
-
-	printf("[AVANT] %s\n", ft_dest);
-	ft_strcpy(ft_dest, av[1]);
-	printf("[APRES] %s\n", ft_dest);
+	ft_strelenTest(av[1]);
+	ft_strcpyTest(av[1]);
 }
